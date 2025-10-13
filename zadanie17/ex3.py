2ef3aaa28d20dee161c9d9f61baf8ff762da305c
nikita <vlasovnikita399@gmail.com>
2025-10-13 18:36:59 +0300

    ex3 done

---
+++ zadanie17/ex3.py
@@ -0,0 +1,20 @@
+def sum3(n):
+    s = 0
+    while n != 0:
+        s += n % 3
+        n //= 3
+    return s
+
+
+a = [int(i) for i in open('3.txt')]
+max11 = sum3(max(i for i in a if i % 11 == 0))
+counter = 0
+minp = 100000
+for i in range(len(a) - 1):
+    a1, a2 = sum3(a[i]), sum3(a[i + 1])
+    if a1 == max11 or a2 == max11:
+        counter += 1
+        s = a[i] + a[i + 1]
+        if s < minp:
+            minp = s
+print(counter, minp)
